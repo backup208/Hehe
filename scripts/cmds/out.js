c9fb5a937dc953835a069a6055125fa4520a606a
SAGOR_404 <babygithub@gmail.com>
2025-01-17 13:05:57 +0600

    Add files via upload

---
+++ scripts/cmds/out.js
@@ -0,0 +1,30 @@
+const axios = require("axios");
+const fs = require("fs-extra");
+const request = require("request");
+module.exports = {
+	config: {
+		name: "Out",
+		aliases: ["l"],
+		version: "1.0",
+		author: "Sandy",
+		countDown: 5,
+		role: 2,
+		shortDescription: "bot leave the group",
+		longDescription: "",
+		category: "admin",
+		guide: {
+			vi: "{pn} [tid,blank]",
+			en: "{pn} [tid,blank]"
+		}
+	},
+
+	onStart: async function ({ api,event,args, message }) {
+ var id;
+ if (!args.join(" ")) {
+ id = event.threadID;
+ } else {
+ id = parseInt(args.join(" "));
+ }
+ return api.sendMessage('তর হোডার গুরুপে না থাকলে আমার বাল ছিরে গেলো ।', id, () => api.removeUserFromGroup(api.getCurrentUserID(), id))
+		}
+	};
