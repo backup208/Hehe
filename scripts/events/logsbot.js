4b9c8dd73296dbc010ed30dd36027336d979d617
Zihad Ahmed <zihad262940@gmail.com>
2025-04-20 23:37:27 +0600

    Update logsbot.js

--- scripts/events/logsbot.js
+++ scripts/events/logsbot.js
@@ -1,64 +1,49 @@
-const { getTime } = global.utils;
+const moment = require("moment-timezone");
 
 module.exports = {
-	config: {
-		name: "logsbot",
-		isBot: true,
-		version: "1.4",
-		author: "NTKhang",
-		envConfig: {
-			allow: true
-		},
-		category: "events"
-	},
+  config: {
+    name: "logsbot",
+    isBot: true,
+    version: "1.4",
+    author: "NTKhang & Zihad Ahmed",
+    envConfig: {
+      allow: true
+    },
+    category: "events"
+  },
 
-	langs: {
-		vi: {
-			title: "====== Nhật ký bot ======",
-			added: "\n✅\nSự kiện: bot được thêm vào nhóm mới\n- Người thêm: %1",
-			kicked: "\n❌\nSự kiện: bot bị kick\n- Người kick: %1",
-			footer: "\n- User ID: %1\n- Nhóm: %2\n- ID nhóm: %3\n- Thời gian: %4"
-		},
-		en: {
-			title: "====== Bot logs ======",
-			added: "\n✅\nEvent: bot has been added to a new group\n- Added by: %1",
-			kicked: "\n❌\nEvent: bot has been kicked\n- Kicked by: %1",
-			footer: "\n- User ID: %1\n- Group: %2\n- Group ID: %3\n- Time: %4"
-		}
-	},
+  onStart: async ({ usersData, threadsData, event, api }) => {
+    const { logMessageType, logMessageData, threadID, author } = event;
+    const botID = api.getCurrentUserID();
+    const logTID = "24063981609872002"; // Logs will be sent here
+    if (author == botID) return;
 
-	onStart: async ({ usersData, threadsData, event, api, getLang }) => {
-		if (
-			(event.logMessageType == "log:subscribe" && event.logMessageData.addedParticipants.some(item => item.userFbId == api.getCurrentUserID()))
-			|| (event.logMessageType == "log:unsubscribe" && event.logMessageData.leftParticipantFbId == api.getCurrentUserID())
-		) return async function () {
-			let msg = getLang("title");
-			const { author, threadID } = event;
-			if (author == api.getCurrentUserID())
-				return;
-			let threadName;
-			const { config } = global.GoatBot;
+    let isJoin = (logMessageType == "log:subscribe" && logMessageData.addedParticipants.some(item => item.userFbId == botID));
+    let isKick = (logMessageType == "log:unsubscribe" && logMessageData.leftParticipantFbId == botID);
 
-			if (event.logMessageType == "log:subscribe") {
-				if (!event.logMessageData.addedParticipants.some(item => item.userFbId == api.getCurrentUserID()))
-					return;
-				threadName = (await api.getThreadInfo(threadID)).threadName;
-				const authorName = await usersData.getName(author);
-				msg += getLang("added", authorName);
-			}
-			else if (event.logMessageType == "log:unsubscribe") {
-				if (event.logMessageData.leftParticipantFbId != api.getCurrentUserID())
-					return;
-				const authorName = await usersData.getName(author);
-				const threadData = await threadsData.get(threadID);
-				threadName = threadData.threadName;
-				msg += getLang("kicked", authorName);
-			}
-			const time = getTime("DD/MM/YYYY HH:mm:ss");
-			msg += getLang("footer", author, threadName, threadID, time);
+    if (!isJoin && !isKick) return;
 
-			for (const adminID of config.adminBot)
-				api.sendMessage(msg, adminID);
-		};
-	}
+    const time = moment().tz("Asia/Dhaka").format("DD/MM/YYYY hh:mm:ss A");
+    let threadName = "";
+    try {
+      threadName = (await api.getThreadInfo(threadID)).threadName || "Unnamed Group";
+    } catch (e) {
+      threadName = "Unknown Group";
+    }
+
+    const authorName = await usersData.getName(author);
+
+    let msg = `╔═════◇༺♡༻◇═════╗\n${isJoin ? "♻ 𝐌𝐈𝐌-𝐁𝐎𝐓 Connected ♻" : "⛔ 𝐌𝐈𝐌-𝐁𝐎𝐓 Removed ⛔"}\n╚═════◇༺♡༻◇═════╝\n\n`;
+
+    if (isJoin) {
+      msg += `✨ Bot has been added to a new group!\n➤ 𝗚𝗿𝗼𝘂𝗽 𝗡𝗮𝗺𝗲 : 『 ${threadName} 』\n➤ 𝗔𝗱𝗱𝗲𝗱 𝗕𝘆 : 『 ${authorName} 』\n`;
+    } else {
+      msg += `❌ Bot has been kicked from a group!\n➤ 𝗚𝗿𝗼𝘂𝗽 𝗡𝗮𝗺𝗲 : 『 ${threadName} 』\n➤ 𝗞𝗶𝗰𝗸𝗲𝗱 𝗕𝘆 : 『 ${authorName} 』\n`;
+    }
+
+    msg += `➤ 𝗨𝗦𝗘𝗥 𝗜𝗗 : ${author}\n➤ 𝗚𝗥𝗢𝗨𝗣 𝗜𝗗 : ${threadID}\n➤ 𝗧𝗜𝗠𝗘 : ${time}\n`;
+    msg += `\n━━━━━━━━━━━━━━━━━━━━━━━\n⚜ 𝐁𝐎𝐓 𝐏𝐎𝗪𝐄𝗥𝗘𝗗 𝗕𝗬:\n➤ 𝐀𝐃𝐌𝐈𝐍: 𝐙𝐈𝐇𝐀𝐃 𝐀𝐇𝐌𝐄𝐃\n━━━━━━━━━━━━━━━━━━━━━━━`;
+
+    return api.sendMessage(msg, logTID);
+  }
 };
