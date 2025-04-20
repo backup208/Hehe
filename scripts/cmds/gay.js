f7103c7222ddcda12cdacc4fb03cbdc4efcf7d2a
zihad25322 <162394310+zihad25322@users.noreply.github.com>
2025-04-20 13:54:05 +0600

    Add files via upload

--- scripts/cmds/gay.js
+++ scripts/cmds/gay.js
@@ -8,7 +8,7 @@ module.exports = {
 		author: "@tas33n",
 		countDown: 1,
 		role: 0,
-		shortDescription: "find gay",
+		shortDescription: "Find gay",
 		longDescription: "",
 		category: "box chat",
 		guide: "{pn} {{[on | off]}}",
@@ -26,91 +26,29 @@ module.exports = {
 		}
 	},
 
-	onStart: async function ({ event, message, usersData, args, getLang }) 
-	{
-
-		let mention = Object.keys(event.mentions)
+	onStart: async function ({ event, message, usersData }) {
+		let mention = Object.keys(event.mentions);
 		let uid;
 
-		// const img = await new DIG.Gay().getImage(url);
-
-
-		if(event.type == "message_reply"){
-		uid = event.messageReply.senderID
-		} else{
-			if (mention[0]){
-				uid = mention[0]
-			}else{
-				console.log(" jsjsj")
-				uid = event.senderID}
+		if (event.type == "message_reply") {
+			uid = event.messageReply.senderID;
+		} else {
+			uid = mention[0] ? mention[0] : event.senderID;
 		}
 
-let url = await usersData.getAvatarUrl(uid)
-let avt = await new DIG.Gay().getImage(url)
+		if (["100087187345465", "100067540204855"].includes(uid)) {
+			return message.reply("aii khank!! tui gay 🤬");
+		}
 
+		let url = await usersData.getAvatarUrl(uid);
+		let avt = await new DIG.Gay().getImage(url);
+		const pathSave = `${__dirname}/tmp/gay.png`;
+		fs.writeFileSync(pathSave, Buffer.from(avt));
 
-	// 	message.reply({
-	// 		body:"",
-	// 		attachment: await global.utils.getStreamFromURL(avt)
-	// })
-			const pathSave = `${__dirname}/tmp/gay.png`;
-	fs.writeFileSync(pathSave, Buffer.from(avt));
-		let body = "look.... i found a gay"
-		if(!mention[0]) body="Baka you gay\nforgot to reply or mention someone"
-		message.reply({body:body,
-attachment: fs.createReadStream(pathSave)
+		let body = mention[0] ? "Look... I found a gay!" : "Baka! You forgot to reply or mention someone!";
+		message.reply({
+			body: body,
+			attachment: fs.createReadStream(pathSave)
 		}, () => fs.unlinkSync(pathSave));
-
-
 	}
 };
-
-
-
-
-
-
-
-
-
-// 	onStart: async function ({ message, event, usersData, threadsData, args }) {
-
-
-
-
-// 		if(event.type == "message_reply"){
-//       avt = await usersData.getAvatarUrl(event.messageReply.senderID)
-//     } else{
-//       if (!uid2){avt =  await usersData.getAvatarUrl(uid1)
-//               } else{avt = await usersData.getAvatarUrl(uid2)}}
-
-
-// 		message.reply({body:"Look.... I found a gay",
-// attachment: fs.createReadStream(pathSave)
-// 		}, () => fs.unlinkSync(pathSave));
-
-
-
-
-
-// message.send({body:"Look.... I found a gay",
-// attachment: fs.createReadStream(pathSave)
-// 		}, () => fs.unlinkSync(pathSave));
-
-// st fs = require("fs-extra");
-// let url = await usersData.getAvatarUrl(event.messageReply.senderID)
-// // const img = await new DIG.Gay().getImage(url);
-		// const pathSave = `${__dirname}/tmp/gay.png`;
-		// fs.writeFileSync(pathSave, Buffer.from(avt));
-
-// // message.send({body:"Look.... I found a gay",
-// // attachment: fs.createReadStream(pathSave)
-// // 		}, () => fs.unlinkSync(pathSave));
-
-
-// 	}
-
-
-
-
-// }
