73459fc564a2f4668e7a4bb964356752a88f6646
Zihad Ahmed <zihad262940@gmail.com>
2025-04-29 20:29:48 +0600

    Update 😒.js

--- scripts/cmds/😒.js
+++ scripts/cmds/😒.js
@@ -14,7 +14,7 @@ module.exports = {
  onChat: async function({ event, message, getLang }) {
  if (event.body && event.body.toLowerCase() === "😒") {
  return message.reply({
- body: "     「𝗕𝗢𝗧 𝗢𝗪𝗡𝗘𝗥\n𝗦𝗔𝗚𝗢𝗥」",
+ body: "     「ADMIN-ZIHAD」",
  attachment: await global.utils.getStreamFromURL("https://drive.google.com/uc?id=16t1NJeGsVVwflXzAW8zIdMmRFkwqFZvr")
  });
  }
