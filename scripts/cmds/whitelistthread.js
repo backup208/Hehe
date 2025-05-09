b751b3d79fddd4aecd0e26ffbf9c47c28a748988
Zihad Ahmed <zihad262940@gmail.com>
2025-05-09 23:37:24 +0600

    Update whitelistthread.js

--- scripts/cmds/whitelistthread.js
+++ scripts/cmds/whitelistthread.js
@@ -4,9 +4,9 @@ const { writeFileSync } = require("fs-extra");
 module.exports = {
 	config: {
 		name: "whitelistthread",
-    aliases: ["wlt"],
+		aliases: ["wlt"],
 		version: "1.5",
-		author: "NTKhang",
+		author: "NTKhang + Modified by Mim",
 		countDown: 5,
 		role: 0,
 		longDescription: {
@@ -14,87 +14,82 @@ module.exports = {
 		},
 		category: "owner",
 		guide: {
-			en: '   {pn} [add | -a] <tid>: Add whiteListThreadIds role for thread'
-				+ '\n	  {pn} [remove | -r] <tid>: Remove whiteListThreadIds role of thread'
-				+ '\n	  {pn} [list | -l]: List all whiteListThreadIds'
+			en: '   {pn} [add | -a]: Add current thread to whitelist'
+				+ '\n   {pn} [remove | -r]: Remove current thread from whitelist'
+				+ '\n   {pn} [add | -a] <tid>: Add specific thread'
+				+ '\n   {pn} [remove | -r] <tid>: Remove specific thread'
+				+ '\n   {pn} [list | -l]: List all whitelisted threads'
 		}
 	},
 
 	langs: {
-	en: {
-		added: "✅ | Added whiteListThreadIds role for %1 thread:\n%2",
-		alreadyAdmin: "\n⚠️ | %1 thread already have whiteListThreadIds role:\n%2",
-		missingIdAdd: "⚠️ | Please enter TID to add whiteListThreadIds role",
-		removed: "✅ | Removed whiteListThreadIds role of %1 thread:\n%2",
-		notAdmin: "⚠️ | %1 users don't have whiteListIds role:\n%2",
-		missingIdRemove: "⚠️ | Please enter TID to remove whiteListThreadIds role",
-		listAdmin: "👑 | List of whiteListThreadIds:\n%1"
+		en: {
+			added: "✅ | Added whiteListThreadIds role for %1 thread:\n%2",
+			alreadyAdmin: "\n⚠️ | %1 thread already have whiteListThreadIds role:\n%2",
+			missingIdAdd: "⚠️ | Please enter TID to add whiteListThreadIds role",
+			removed: "✅ | Removed whiteListThreadIds role of %1 thread:\n%2",
+			notAdmin: "⚠️ | %1 users don't have whiteListIds role:\n%2",
+			missingIdRemove: "⚠️ | Please enter TID to remove whiteListThreadIds role",
+			listAdmin: "👑 | List of whiteListThreadIds:\n%1"
 		}
 	},
 
-	onStart: async function ({ message, args, usersData, event, getLang, api }) {
+	onStart: async function ({ message, args, usersData, event, getLang }) {
 		switch (args[0]) {
 			case "add":
 			case "-a":
-            case "+": {
-				if (args[1]) {
-					let uids = [];
-					if (uids.push(event.threadID));
-					else
-						uids = args.filter(arg => !isNaN(arg));
-					const notAdminIds = [];
-					const authorIds = [];
-					for (const uid of uids) {
-						if (config.whiteListModeThread.whiteListThreadIds.includes(uid))
-							authorIds.push(uid);
-						else
-							notAdminIds.push(uid);
-					}
+			case "+": {
+				const uids = args[1] ? args.filter(arg => !isNaN(arg)) : [event.threadID];
+				const alreadyInList = [], addedNow = [];
 
-					config.whiteListModeThread.whiteListThreadIds.push(...notAdminIds);
-					const getNames = await Promise.all(uids.map(uid => usersData.getName(uid).then(name => ({ uid, name }))));
-					writeFileSync(global.client.dirConfig, JSON.stringify(config, null, 2));
-					return message.reply(
-						(notAdminIds.length > 0 ? getLang("added", notAdminIds.length, getNames.map(({ uid, name }) => `• ${name} (${uid})`).join("\n")) : "")
-						+ (authorIds.length > 0 ? getLang("alreadyAdmin", authorIds.length, authorIds.map(uid => `• ${uid}`).join("\n")) : "")
-					);
+				for (const uid of uids) {
+					if (config.whiteListModeThread.whiteListThreadIds.includes(uid))
+						alreadyInList.push(uid);
+					else {
+						config.whiteListModeThread.whiteListThreadIds.push(uid);
+						addedNow.push(uid);
+					}
 				}
-				else
-					return message.reply(getLang("missingIdAdd"));
+
+				writeFileSync(global.client.dirConfig, JSON.stringify(config, null, 2));
+				const getNames = await Promise.all(uids.map(uid => usersData.getName(uid).then(name => ({ uid, name }))));
+
+				return message.reply(
+					(addedNow.length > 0 ? getLang("added", addedNow.length, getNames.filter(i => addedNow.includes(i.uid)).map(({ uid, name }) => `• ${name} (${uid})`).join("\n")) : "")
+					+ (alreadyInList.length > 0 ? getLang("alreadyAdmin", alreadyInList.length, alreadyInList.map(uid => `• ${uid}`).join("\n")) : "")
+				);
 			}
+
 			case "remove":
 			case "-r":
-            case "-": {
-              if (args[1]) {
-                let uids = [];
-                if (uids.push(event.threadID));
-                else
-                  uids = args.filter(arg => !isNaN(arg));
-					const notAdminIds = [];
-					const authorIds = [];
-					for (const uid of uids) {
-						if (config.whiteListModeThread.whiteListThreadIds.includes(uid))
-							authorIds.push(uid);
-						else
-							notAdminIds.push(uid);
-					}
-					for (const uid of authorIds)
+			case "-": {
+				const uids = args[1] ? args.filter(arg => !isNaN(arg)) : [event.threadID];
+				const notInList = [], removedNow = [];
+
+				for (const uid of uids) {
+					if (config.whiteListModeThread.whiteListThreadIds.includes(uid)) {
 						config.whiteListModeThread.whiteListThreadIds.splice(config.whiteListModeThread.whiteListThreadIds.indexOf(uid), 1);
-					const getNames = await Promise.all(authorIds.map(uid => usersData.getName(uid).then(name => ({ uid, name }))));
-					writeFileSync(global.client.dirConfig, JSON.stringify(config, null, 2));
-					return message.reply(
-						(authorIds.length > 0 ? getLang("removed", authorIds.length, getNames.map(({ uid, name }) => `• ${name} (${uid})`).join("\n")) : "")
-						+ (notAdminIds.length > 0 ? getLang("notAdmin", notAdminIds.length, notAdminIds.map(uid => `• ${uid}`).join("\n")) : "")
-					);
+						removedNow.push(uid);
+					} else {
+						notInList.push(uid);
+					}
 				}
-				else
-					return message.reply(getLang("missingIdRemove"));
+
+				writeFileSync(global.client.dirConfig, JSON.stringify(config, null, 2));
+				const getNames = await Promise.all(removedNow.map(uid => usersData.getName(uid).then(name => ({ uid, name }))));
+
+				return message.reply(
+					(removedNow.length > 0 ? getLang("removed", removedNow.length, getNames.map(({ uid, name }) => `• ${name} (${uid})`).join("\n")) : "")
+					+ (notInList.length > 0 ? getLang("notAdmin", notInList.length, notInList.map(uid => `• ${uid}`).join("\n")) : "")
+				);
 			}
+
 			case "list":
 			case "-l": {
 				const getNames = await Promise.all(config.whiteListModeThread.whiteListThreadIds.map(uid => usersData.getName(uid).then(name => ({ uid, name }))));
 				return message.reply(getLang("listAdmin", getNames.map(({ uid, name }) => `• ${name} (${uid})`).join("\n")));
 			}
+
 			default:
 				return message.SyntaxError();
 		}
