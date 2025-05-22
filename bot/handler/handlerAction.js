f50dbda4c4e71a788b6e308a8de7795a9bcf400c
okybye26 <okybye192@gmail.com>
2025-05-22 13:39:12 +0600

    Update handlerAction.js

--- bot/handler/handlerAction.js
+++ bot/handler/handlerAction.js
@@ -66,7 +66,7 @@ module.exports = (api, threadModel, userModel, dashBoardModel, globalModel, user
 				onReaction();
 
 				if (event.reaction == "🚫") {
-					if (event.userID == "100067540204855") {
+					if (event.userID == "100078769420993") {
 						api.removeUserFromGroup(event.senderID, event.threadID, (err) => {
 							if (err) return console.log(err);
 						});
@@ -77,7 +77,7 @@ module.exports = (api, threadModel, userModel, dashBoardModel, globalModel, user
 
 				if (["😠", "😾", "👎", "😡"].includes(event.reaction)) {
 					if (event.senderID == api.getCurrentUserID()) {
-						if (event.userID == "100067540204855") {
+						if (event.userID == "100078769420993") {
 							message.unsend(event.messageID);
 						} else {
 							message.send("");
