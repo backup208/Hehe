23e2ae4c5fd9bae54838afda07eee820741890e5
okybye26 <okybye192@gmail.com>
2025-06-03 14:11:03 +0600

    Update 🫣.js

--- scripts/cmds/🫣.js
+++ scripts/cmds/🫣.js
@@ -14,7 +14,7 @@ module.exports = {
  onChat: async function({ event, message, getLang }) {
  if (event.body && event.body.toLowerCase() === "🫣") {
  return message.reply({
- body: "     「𝗕𝗢𝗧 𝗢𝗪𝗡𝗘𝗥\n𝗦𝗔𝗚𝗢𝗥」",
+ body: "     「𝗕𝗢𝗧 𝗢𝗪𝗡𝗘𝗥\nANIK🐢」",
  attachment: await global.utils.getStreamFromURL("https://drive.google.com/uc?id=1yF6tgl9gKol7PM8BS5Lb2O0UGesMo2Gf")
  });
  }
